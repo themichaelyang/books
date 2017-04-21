713ea37409e72355101ba18eede039408df904a6
Michael Yang <themichaelyang@gmail.com>
2017-04-21 03:11:30 -0400

    added gulp build

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const typeset = require('gulp-typeset');
+const sass = require('gulp-sass');
+// const concat = require('gulp-concat');
+
+const src = 'src/';
+const dist = 'dist/';
+
+const paths = {
+  scss: src + '**/*.scss',
+  html: src + '**/*.html'
+}
+
+gulp.task('html', () => {
+  return gulp.src(paths.html)
+    .pipe(typeset())
+    .pipe(gulp.dest(dist));
+});
+
+gulp.task('scss', () => {
+  return gulp.src(paths.scss)
+    .pipe(sass().on('error', sass.logError))
+    // .pipe(concat('style.css'))
+    .pipe(gulp.dest(dist));
+});
+
+gulp.task('default', ['scss', 'html']);
+
+gulp.task('watch', ['default'], () => {
+  gulp.watch(paths.scss, ['scss']);
+  gulp.watch(paths.html, ['html']);
+});
+
+gulp.task('watch-all', ['default'], () => {
+  gulp.watch(src, ['default']);
+});
